18796c3bc885d20956f87fe3071cc8ba440f1d0c
Markus Kont <alias013@gmail.com>
2019-06-16 07:49:05 +0000

    need multimon + my quality of life

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "hack:size=9" };
+static const char dmenufont[]       = "hack:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -94,7 +94,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,			XK_q,		killclient,	{0} },
+	{ MODKEY|ShiftMask,			XK_q,		killclient,	{0} },
 	{ MODKEY,                       XK_j,		focusstack,	{.i = +1 } },
 	{ MODKEY,                       XK_k,		focusstack,	{.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,		movestack,	{.i = +1 } },
@@ -102,10 +102,10 @@ static Key keys[] = {
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY,                       XK_h,		setmfact,	{.f = -0.05} },
 	{ MODKEY,                       XK_l,		setmfact,	{.f = +0.05} },
+
 	{ MODKEY|ShiftMask,		XK_i,		incnmaster,	{.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_o,		incnmaster,	{.i = -1 } },
-	{ MODKEY,			XK_b,		togglebar,	{0} },
-	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
+	{ MODKEY,			XK_b,		togglebar,	{0} }, { MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
 	{ MODKEY,			XK_f,		setlayout,	{.v = &layouts[1]} },
 	{ MODKEY,			XK_y,		setlayout,	{.v = &layouts[2]} },
 	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[3]} },
@@ -115,10 +115,10 @@ static Key keys[] = {
 	{ MODKEY,			XK_backslash,	view,		{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
-	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
-	/* { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, */
-	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(			XK_1,			0)
 	TAGKEYS(			XK_2,			1)
 	TAGKEYS(			XK_3,			2)
@@ -130,7 +130,6 @@ static Key keys[] = {
 	TAGKEYS(			XK_9,			8)
 	{ MODKEY,                       XK_0,		view,		{.ui = ~0 } },
 	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
-	{ MODKEY,			XK_F2,		quit,		{0} },
 	{ MODKEY,			XK_g,		shiftview,	{ .i = -1 } },
 	{ MODKEY,			XK_semicolon,	shiftview,	{ .i = 1 } },
 	{ MODKEY,			XK_Page_Up,	shiftview,	{ .i = -1 } },
